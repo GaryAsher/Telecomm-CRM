b3ae8214c1f65bb42050845571bc95ed7e35ff13
Gary <181423616+GaryAsher@users.noreply.github.com>
2026-02-12 09:36:17 -0600

    Delete telecrom-app directory

--- telecrom-app/src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-  return {
-    session: locals.session,
-    user: locals.user,
-    permissions: locals.permissions
-  };
-};
